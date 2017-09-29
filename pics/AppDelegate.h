f326698e8796f9701529f650fed0283aef70c914
Anand Biligiri <abiligiri@users.noreply.github.com>
2017-09-29 00:21:18 -0700

    Initial Commit

---
+++ pics/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pics
+//
+//  Created by  Anand Biligiri on 9/22/17.
+//  Copyright © 2017  Anand Biligiri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pics/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pics
+//
+//  Created by  Anand Biligiri on 9/22/17.
+//  Copyright © 2017  Anand Biligiri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
